c3d539198ee1c723e0df2dc1e7248927899572c0
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-08-16 16:33:38 -0500

    se quita validacion de token

--- src/app/dashboard/services/dashboard.service.ts
+++ src/app/dashboard/services/dashboard.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class DashboardService {
 private url: string = 'http://localhost:8080/api/v1/';
-  constructor(private http: HttpClient) {
+private token: string = "Bearer " + localStorage.getItem("token"); //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZW8iOiIyQDIuY29tIiwidWlkIjoic00yMV8yMl9JNTUiLCJyb2wiOjEsImlhdCI6MTY1MjcyNzExNSwiZXhwIjoxNjg0Mjg0NzE1fQ.mjpOc7b_BnB6ITkzqO9KigWQM7i6ln5OBXDLkcC5Tys';
+private headers: HttpHeaders = new HttpHeaders({ Authorization: this.token });
+  constructor(private http: HttpClient,) {
 
 
    
