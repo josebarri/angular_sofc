855f5255061e2529d8a94ed5c7fe9778c414daaf
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-09-07 23:50:49 -0500

    ajueste de mas

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import {DashboardService} from '../../services/dashboard.service';
+import { Component, OnInit, inject   } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
-export default class ControlFlowComponent {
+export default class ControlFlowComponent implements OnInit {
+  constructor(
+    private dashboardService:DashboardService, private router: Router) { }
+  puntos: any[] = [];
+  puntos1: any[] = [];
+  puntos2: any[] = [];
 
+  
+  ngOnInit(): void {
+   this.getMascotas()
+   this.getDueños()
+   this.getPaciente()
+  }
+  getMascotas(){
+    this.dashboardService.getMascotas().subscribe((resp) => {
+      this.puntos= resp.data;
+    console.log('mis mascotas', this.puntos);
+    
+    });
+  }
+  getDueños(){
+    this.dashboardService.getDueno().subscribe((resp) => {
+      this.puntos1= resp.data;
+    console.log('mis dueños', this.puntos1);
+    
+    });
+  }
+  getPaciente(){
+    this.dashboardService.getPaciente().subscribe((resp) => {
+      this.puntos2= resp.data;
+    console.log('mis pacientes', this.puntos2);
+    
+    });
+  }
 }
